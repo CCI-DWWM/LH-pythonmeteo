2a7e2a05352bb11e7fad70c37b0dc62ad3b27ca5
louis.hameau <louis.hameau@campus-centre.fr>
2025-07-23 09:48:23 +0200

    Commit

--- creer_bdd.py
+++ creer_bdd.py
@@ -8,22 +8,22 @@ cur = conn.cursor()
 # On supprime la table si elle existe
 cur.execute("DROP TABLE IF EXISTS villes")
 
-# On crée une table simple avec cp (code postal) et nom_commune
+# On crée une table avec code postal et nom_commune
 cur.execute("CREATE TABLE villes (cp TEXT, nom_commune TEXT)")
 
-# Lecture du fichier CSV avec encodage latin-1 et séparateur ;
+# Lecture du fichier CSV;
 with open("019HexaSmal.csv", newline='', encoding='latin-1') as fichier:
     reader = csv.DictReader(fichier, delimiter=';')
 
     for ligne in reader:
         nom = ligne['Nom_de_la_commune'].strip()
-        cp = ligne['Code_postal'].zfill(5)  # remet les zéros au début si nécessaire
+        cp = ligne['Code_postal'].zfill(5)
 
         if cp and nom:
             cur.execute("INSERT INTO villes VALUES (?, ?)", (cp, nom))
 
-# Sauvegarde et fermeture
+
 conn.commit()
 conn.close()
 
-print("Base de données villes.sqlite créée avec succès ✅")
+print("Base de données villes.sqlite créée avec succès")
